6af7730f1b576dbcaa2cf6f63b7832e757a04de9
vivirbr <vivirbr.dsc@gmail.com>
2023-10-09 15:28:31 -0700

    final - done :)

--- app.R
+++ app.R
@@ -1,3 +1,4 @@
+# Loading packages
 library(httr)
 library(rjson)
 library(shiny)
@@ -5,63 +6,68 @@ library(readr)
 library(dplyr)
 library(shinythemes)
 
-options(shiny.host = "0.0.0.0")
-options(shiny.port = 5000)
+# Setting a very simple page with a left panel with the input for the prompt
+# the output of the function will return as a table in the right panel
+# The action button Run will allow you to control the function and require a new run of the prompt
 
 ui <- fluidPage(
   theme = shinytheme("slate"),
   fluidRow(
     column(4, wellPanel(
-      textInput("title", "Potential title:", "Disentangling the numbers behind agriculture-driven tropical
-deforestation"),
-      textInput("key", "Key words:", ""), 
-      textInput("sentence", "Teaser:", "A Review disentangles the numbers behind agriculture-driven deforestation and
-explains the different forms it can take."), 
       br(),
-      actionButton("goButton", "Run")
+      textInput("title", "Potential title:", "Disentangling the numbers behind agriculture-driven tropical deforestation"),
+      textInput("key", "Key words:", ""), 
+      textInput("sentence", "Teaser:", "A Review disentangles the numbers behind agriculture-driven deforestation and explains the different forms it can take."), 
+      br(),
+      actionButton("searchButton", "Search")
     )),
     column(8,
       h4("List of the best journals according to the information provided:"),
-        # span(textOutput("answer"), style="color:white font-size:large")
         tableOutput("answer")
     )
   )
 )
 
+# Server function holds the main application feature - isolate guarantees that it runs only after input searchButton is activated
+# Because I am using here a simple Sys.getenv - you will have to add it as R environment in the apps directory
+
 server <- function(input,output) {
   
-output$answer <- renderTable({
-    input$goButton
-    
-    isolate({
-      # Set the PaLM API URL and headers.
-      url = paste0("https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=",Sys.getenv("GC_KEY"))
-      headers <-list("Content-Type" = "application/json")
+  output$answer <- renderTable({
+      input$searchButton
+      
+      isolate({
+        # Set the PaLM API URL and headers.
+        url = paste0("https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=",Sys.getenv("GC_KEY"))
+        headers <-list("Content-Type" = "application/json")
 
-      # Create the request body.
-      list_object <- list()
-      list_object$prompt <- list(text = paste0("Return a table with a maximum of eigth of the most suitable scientific journals 
-                                                for paper publication, and their respective impact factors and link to the journal
-                                                website. Consider how the journal scope is aligned to a paper with a potential title '",
-                                               input$title,
-                                               "' consider that the main key words are '",
-                                               input$key,
-                                               "' and also that a very short description of the paper is '",
-                                               input$sentence,
-                                               "'."))
-      list_object$temperature <- 0.5
-      list_object$candidateCount <- 2
+        # Create the request body.
+        list_object <- list()
+        list_object$prompt <- list(text = paste0("Return a table with eigth rows, each one containting the most suitable scientific journal 
+                                                  for paper publication. The first column will be the name of the journal, the second column
+                                                  will be the impact factor, the third column will be the link t the journal webpage.
+                                                  To evaluate the most suitable journal, consider how the journal scope and previous publications
+                                                  are aligned to a paper which the potential title is '",
+                                                  input$title,
+                                                  "', the main key words are '",
+                                                  input$key,
+                                                  "', and the paper's sentence teaser is '",
+                                                  input$sentence,
+                                                  "'."))
+        list_object$temperature <- 0.1 # Setting a more restrictive model temperature to avoid top journals to be repeated every time
+        list_object$candidateCount <- 1 # Candidate is already set as 1 considering that the table produced will always return top eight journals
 
-      body = toJSON(list_object)
+        body = toJSON(list_object) # It is important that the body is a json instead of a simple list
 
-      # Send the request and get the response.
-      response <- httr::POST(url, headers = headers, body = body, encode = "json")
-      json <- fromJSON(rawToChar(response$content))
-      tab <- data.frame(read_delim(json$candidates[[1]]$output, delim = '|', col_names=TRUE, show_col_types = FALSE))
-      tab <- tab %>% select(where(~!all(is.na(.))))
-      colnames(tab) <- gsub(colnames(tab), pattern = "X...|\\...", replacement = "")  
+        response <- httr::POST(url, headers = headers, body = body, encode = "json") # Send the request and get the response.
+        json <- fromJSON(rawToChar(response$content)) # Returning the response content
 
-      tab <- tab[-1,]
+        # reading the only answer provided in a data.frame format
+        tab <- data.frame(read_delim(json$candidates[[1]]$output, delim = '|', col_names=TRUE, show_col_types = FALSE)) 
+        tab <- tab %>% select(where(~!all(is.na(.)))) # removing columns that are purely NAs
+        # colnames(tab) <- gsub(colnames(tab), pattern = "X...|\\...", replacement = "")  
+        colnames(tab) <- c("Journal","Impact Factor", "Link")
+        tab <- tab[-1,] #removing first empty row
 
     })
 
---
+++ deploy.md
@@ -0,0 +1,4 @@
+### Deploy using Shiny 
+
+To deploy your shiny app follow instructions on 
+https://www.shinyapps.io/admin/#/dashboard
